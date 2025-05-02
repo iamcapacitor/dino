a1830a10f6bc450b5e2199ac113c0f4c92551d81
Capacitor <rohitkhengare172@gmail.com>
2025-05-02 11:08:56 +0530

    this is version 2 with highest score in localstorage

--- script.js
+++ script.js
@@ -4,22 +4,28 @@ let cross = true;
 let restart = document.querySelector('.restart');
 let gamedone = document.querySelector('.gamedone');
 let mainscore = document.querySelector('.mainscore');
+let keycode;
 let refreshpage = ()=>{
     window.location.reload();
 };
 
 document.onkeydown = function(e){
-    console.log('key code is' , e.keyCode)
-    keycode = e.keyCode;
-    if((keycode == 38) || (keycode == 87)){
+    // console.log('key code is' , e.key)
+    keycode = e.key;
+    if((keycode == 'ArrowUp') || (keycode == 'w') || (keycode == 'W')){
         dino = document.querySelector('.mainplayer');
-        console.log('i am jumping');
+        // console.log('i am jumping');
         dino.classList.add('jump');
         setTimeout(()=>{
             dino.classList.remove('jump');
         },400);
-    }else if(keycode == 32){
+    }else if(keycode == ' '){
         document.querySelector('.right').classList.toggle('pause');
+        if(cross){
+          cross = false
+        }else{
+          cross = true;
+        }
         return
     }
 };
@@ -38,17 +44,27 @@ setInterval(()=>{
 
 // function to update score by 1 digits
 
-setInterval(()=>{
+let maingamelogic = setInterval(()=>{
 
 if((offsetx >= -100) && (offsetx <= 120) && (offsety <=100) ){
+  clearInterval(maingamelogic);
     document.querySelector('.right').classList.add('pause');
     dino.style.bottom =`${dy}px`;
     document.querySelector('.right').style.left = `${offsetx}px`;
-    document.querySelector('.gamedone').innerHTML = 'Game Over ! Good Score';
+    
+   
     document.querySelector('.restart').style.visibility = 'visible';
     document.querySelector('.right').style.animationDuration = `${duration}ms`
+    // to store the highest score in cache
+    let highestscore = parseInt(localStorage.getItem('highestscore'));
+    if(highestscore < parseInt(mainscore.innerHTML)){
+      document.querySelector('.gamedone').innerHTML = 'Game Over ! New highest Score';
+      localStorage.setItem("highestscore",mainscore.innerHTML)
+    }else{
+      document.querySelector('.gamedone').innerHTML = 'Game Over ! Good Score';
+    }
     return;
-}else if (keycode !== 32){
+}else if (cross){
     // to increase score
     mainscore.innerHTML = ++score; 
     // to increase speed of game
